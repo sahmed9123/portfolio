7225fee59b31a64ffaff8201b425ae289de66804
sahmed9123 <sahmed21@umd.edu>
2023-10-30 19:58:09 -0400

    adding export to next config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "giphy.com",
-      },
-    ],
-  },
   output: "export",
 };
 
